6212cd6201d6837905b793ce7ee19742d1d5566e
GGolfz <wisarutgolf11@gmail.com>
2021-03-22 21:20:36 +0700

    config: color :wrench:

---
+++ lib/config/color.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+const kNeutralWhite = const Color(0xFFFFFFFF);
+const kNeutral25 = const Color(0xFFFAFAFA);
+const kNeutral50 = const Color(0xFFF5F5F5);
+const kNeutral100 = const Color(0xFFF0F0F0);
+const kNeutral150 = const Color(0xFFEBEBEB);
+const kNeutral200 = const Color(0xFFE0E0E0);
+const kNeutral250 = const Color(0xFFC7C7C7);
+const kNeutral300 = const Color(0xFFB2B2B2);
+const kNeutral350 = const Color(0xFF949494);
+const kNeutral400 = const Color(0xFF8A8A8A);
+const kNeutral450 = const Color(0xFF555555);
+const kNeutral500 = const Color(0xFF424242);
+const kNeutral550 = const Color(0xFF3D3D3D);
+const kNeutral600 = const Color(0xFF333333);
+const kNeutral700 = const Color(0xFF292929);
+const kNeutral800 = const Color(0xFF1E1E1E);
+const kNeutral900 = const Color(0xFF0F0F0F);
+const kNeutralBlack = const Color(0xFF000000);
+const kGold100 = const Color(0xFFFFF9EE);
+const kGold200 = const Color(0xFFF4DF99);
+const kGold400 = const Color(0xFFE4BF45);
+const kGold500 = const Color(0xFFD3AE36);
+const kRed400 = const Color(0xFFE02929);
+const kGreen500 = const Color(0xFF229C45);
+const kTitleGradient = const LinearGradient(
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight,
+    colors: [Color(0xFFA9A5A3), Color(0xFFD4B356)],
+    stops: [0.3, 1]);
