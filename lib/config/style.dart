dcaef199144acbc5080736a0be040fbd72d835c7
GGolfz <wisarutgolf11@gmail.com>
2021-03-23 23:34:39 +0700

    config: constant & text style :wrench:

--- lib/config/style.dart
+++ lib/config/style.dart
@@ -1,3 +1,124 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:setthi/config/color.dart';
+import 'package:setthi/config/constants.dart';
 
-// เดะมาทำ
+final kHeadline1Black = GoogleFonts.quicksand(
+    fontSize: kFontSizeHeadline1,
+    color: kNeutralBlack,
+    fontWeight: FontWeight.bold);
+
+final kHeadline1White = GoogleFonts.quicksand(
+    fontSize: kFontSizeHeadline1,
+    color: kNeutralWhite,
+    fontWeight: FontWeight.bold);
+
+final kHeadline2Black = GoogleFonts.quicksand(
+    fontSize: kFontSizeHeadline2,
+    color: kNeutralBlack,
+    fontWeight: FontWeight.bold);
+
+final kHeadline2White = GoogleFonts.quicksand(
+    fontSize: kFontSizeHeadline2,
+    color: kNeutralWhite,
+    fontWeight: FontWeight.bold);
+
+final kHeadline3Black = GoogleFonts.quicksand(
+    fontSize: kFontSizeHeadline3,
+    color: kNeutralBlack,
+    fontWeight: FontWeight.bold);
+
+final kHeadline3White = GoogleFonts.quicksand(
+    fontSize: kFontSizeHeadline3,
+    color: kNeutralWhite,
+    fontWeight: FontWeight.bold);
+
+final kHeadline4Black = GoogleFonts.quicksand(
+    fontSize: kFontSizeHeadline4,
+    color: kNeutralBlack,
+    fontWeight: FontWeight.w600);
+
+final kHeadline4White = GoogleFonts.quicksand(
+    fontSize: kFontSizeHeadline4,
+    color: kNeutralWhite,
+    fontWeight: FontWeight.w600);
+
+final kHeadline4Red = GoogleFonts.quicksand(
+    fontSize: kFontSizeHeadline4, color: kRed400, fontWeight: FontWeight.w600);
+
+final kHeadline4Green = GoogleFonts.quicksand(
+    fontSize: kFontSizeHeadline4,
+    color: kGreen500,
+    fontWeight: FontWeight.w600);
+
+final kSubtitle1Black = GoogleFonts.quicksand(
+    fontSize: kFontSizeSubtitle1,
+    color: kNeutralBlack,
+    fontWeight: FontWeight.normal);
+
+final kSubtitle1White = GoogleFonts.quicksand(
+    fontSize: kFontSizeSubtitle1,
+    color: kNeutralBlack,
+    fontWeight: FontWeight.normal);
+
+final kSubtitle1Gold = GoogleFonts.quicksand(
+    fontSize: kFontSizeSubtitle1,
+    color: kGold500,
+    fontWeight: FontWeight.normal);
+
+final kSubtitle1Red = GoogleFonts.quicksand(
+    fontSize: kFontSizeSubtitle1,
+    color: kRed400,
+    fontWeight: FontWeight.normal);
+
+final kSubtitle1Green = GoogleFonts.quicksand(
+    fontSize: kFontSizeSubtitle1,
+    color: kGreen500,
+    fontWeight: FontWeight.normal);
+
+final kSubtitle2Black = GoogleFonts.quicksand(
+    fontSize: kFontSizeSubtitle2,
+    color: kNeutralBlack,
+    fontWeight: FontWeight.normal);
+
+final kSubtitle2White = GoogleFonts.quicksand(
+    fontSize: kFontSizeSubtitle2,
+    color: kNeutralBlack,
+    fontWeight: FontWeight.normal);
+
+final kSubtitle2Gold = GoogleFonts.quicksand(
+    fontSize: kFontSizeSubtitle2,
+    color: kGold500,
+    fontWeight: FontWeight.normal);
+
+final kBody1Black = GoogleFonts.quicksand(
+    fontSize: kFontSizeBody1,
+    color: kNeutralBlack,
+    fontWeight: FontWeight.normal);
+
+final kBody1White = GoogleFonts.quicksand(
+    fontSize: kFontSizeBody1,
+    color: kNeutralWhite,
+    fontWeight: FontWeight.normal);
+
+final kBody1Gold = GoogleFonts.quicksand(
+    fontSize: kFontSizeBody1, color: kGold500, fontWeight: FontWeight.normal);
+
+final kBody1Red = GoogleFonts.quicksand(
+    fontSize: kFontSizeBody1, color: kRed200, fontWeight: FontWeight.normal);
+
+final kBody2Black = GoogleFonts.quicksand(
+    fontSize: kFontSizeBody2,
+    color: kNeutralBlack,
+    fontWeight: FontWeight.normal);
+
+final kBody2White = GoogleFonts.quicksand(
+    fontSize: kFontSizeBody2,
+    color: kNeutralWhite,
+    fontWeight: FontWeight.normal);
+
+final kBody2Gold = GoogleFonts.quicksand(
+    fontSize: kFontSizeBody2, color: kGold500, fontWeight: FontWeight.normal);
+
+final kBody2Red = GoogleFonts.quicksand(
+    fontSize: kFontSizeBody2, color: kRed200, fontWeight: FontWeight.normal);
