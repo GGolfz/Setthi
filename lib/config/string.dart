491e51a84eddfac2f04ba4f1ebf1435f024eac88
GGolfz <wisarutgolf11@gmail.com>
2021-03-23 23:37:10 +0700

    config: string text :wrench:

--- lib/config/string.dart
+++ lib/config/string.dart
@@ -1,3 +1,10 @@
 const appName = "SETTHI";
 const descriptionText =
     "Application to help you manage manage your expenses and transactions in your daily life and also allow you to split your money by the purpose.";
+const forgetText = "Forget password ?";
+const suggestRegisterQuestion = "New here ?";
+const suggestRegisterText = "Create Account";
+const suggestSigninQuestion = "Already have an account ?";
+const suggestSigninText = "Sign In";
+const createWalletText = "Create a new wallet";
+const createBudgetText = "Create a new budget";
