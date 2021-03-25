3b44c5f787524784a6509e81f48c393e10540003
GGolfz <wisarutgolf11@gmail.com>
2021-03-25 22:49:05 +0700

    update: calendar color :sparkles:

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:setthi/config/color.dart';
 import 'package:setthi/provider/authenicateProvider.dart';
 import './screens/landingScreen.dart';
 import './screens/mainScreen.dart';
@@ -22,8 +23,11 @@ class SetthiApp extends StatelessWidget {
       child: Consumer<AuthenticateProvider>(
         builder: (ctx, auth, _) => MaterialApp(
           navigatorKey: navigatorKey,
+          theme: ThemeData(
+              primaryColor: kGold500,
+              colorScheme: ColorScheme.light().copyWith(primary: kGold500)),
           title: 'Setthi',
-          home: auth.isAuth ? MainScreen() : LandingScreen(),
+          home: !auth.isAuth ? MainScreen() : LandingScreen(),
         ),
       ),
     );
