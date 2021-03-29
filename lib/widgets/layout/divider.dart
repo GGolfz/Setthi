165b082951897916de14aad46b15840356d0d023
GGolfz <wisarutgolf11@gmail.com>
2021-03-29 20:04:44 +0700

    add: custom divider :sparkles:

---
+++ lib/widgets/layout/divider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:setthi/config/color.dart';
+
+class CustomDivider extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Divider(
+      color: kNeutral200,
+    );
+  }
+}
