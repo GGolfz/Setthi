7386b22a01d35358a0b2fc911ac3e66d2618ff04
GGolfz <wisarutgolf11@gmail.com>
2021-03-29 11:03:12 +0700

    refactor: splash screen :sparkles:

--- lib/screens/splashScreen.dart
+++ lib/screens/splashScreen.dart
@@ -15,86 +15,97 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen>
     with TickerProviderStateMixin {
-  var isStart = false;
-  var renderText = false;
-  var height = kSizeXL + 60;
-  AnimationController animationController1;
-  AnimationController animationController2;
+  var _isLaunch = false;
+  var _renderText = false;
+  var _height = kSizeXL + kSizeL;
+  AnimationController _animationController1;
+  AnimationController _animationController2;
   @override
   void initState() {
     super.initState();
     loadWidget();
-    animationController1 =
+    _animationController1 =
         AnimationController(vsync: this, duration: Duration(seconds: 1));
-    animationController2 =
+    _animationController2 =
         AnimationController(vsync: this, duration: Duration(seconds: 5));
-    animationController1.forward();
-    animationController2.forward();
+    _animationController1.forward();
+    _animationController2.forward();
   }
 
   @override
   void dispose() {
     super.dispose();
-    animationController1.dispose();
-    animationController2.dispose();
+    _animationController1.dispose();
+    _animationController2.dispose();
   }
 
   void animatedUp() {
-    setState(() {
-      height = kSizeXL;
+    Timer(Duration(seconds: 1, milliseconds: 500), () {
+      setState(() {
+        _height = kSizeXL;
+      });
+    });
+  }
+
+  void showText() {
+    Timer(Duration(seconds: 3, milliseconds: 250), () {
+      setState(() {
+        _renderText = true;
+      });
+    });
+  }
+
+  void launchScreen() {
+    Timer(Duration(seconds: 5), () {
+      setState(() {
+        _isLaunch = true;
+      });
     });
   }
 
   void loadWidget() async {
-    Timer(Duration(seconds: 1, milliseconds: 500), () {
-      animatedUp();
-    });
-    Timer(Duration(seconds: 3, milliseconds: 250), () {
-      setState(() {
-        renderText = true;
-      });
-    });
-    Timer(Duration(seconds: 5), () {
-      setState(() {
-        isStart = true;
-      });
-    });
+    animatedUp();
+    showText();
+    launchScreen();
+  }
+
+  Animation<double> tweenAnimation(controller) {
+    return Tween(begin: 0.0, end: 1.0).animate(controller);
+  }
+
+  Widget fadeWidget(animation, widget) {
+    return FadeTransition(
+      opacity: animation,
+      child: widget,
+    );
+  }
+
+  Widget splashWidget(animation1, animation2) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: kSizeS, horizontal: kSizeM),
+      width: double.infinity,
+      height: double.infinity,
+      color: kGold100,
+      child: Column(children: [
+        AnimatedContainer(
+          duration: Duration(seconds: 1, milliseconds: 500),
+          height: _height,
+          child: Container(
+            color: Colors.transparent,
+            height: kSizeXL + kSizeL,
+            width: double.infinity,
+          ),
+        ),
+        fadeWidget(animation1, LogoImage()),
+        _renderText ? fadeWidget(animation1, LogoText()) : kSizedBoxXXS,
+      ]),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    final animation1 =
-        Tween(begin: 0.0, end: 1.0).animate(animationController1);
-    final animation2 =
-        Tween(begin: 0.0, end: 1.0).animate(animationController2);
-    return !isStart
-        ? Container(
-            padding: EdgeInsets.symmetric(vertical: kSizeS, horizontal: kSizeM),
-            width: double.infinity,
-            height: double.infinity,
-            color: kGold100,
-            child: Column(children: [
-              AnimatedContainer(
-                duration: Duration(seconds: 1, milliseconds: 500),
-                height: height,
-                child: Container(
-                  color: Colors.transparent,
-                  height: kSizeXL + 60,
-                  width: double.infinity,
-                ),
-              ),
-              FadeTransition(
-                opacity: animation1,
-                child: LogoImage(),
-              ),
-              renderText
-                  ? FadeTransition(
-                      opacity: animation2,
-                      child: LogoText(),
-                    )
-                  : kSizedBoxXXS,
-            ]),
-          )
-        : widget.nextScreen;
+    final animation1 = tweenAnimation(_animationController1);
+    final animation2 = tweenAnimation(_animationController2);
+    return _isLaunch ? widget.nextScreen : splashWidget(animation1, animation2);
   }
 }
