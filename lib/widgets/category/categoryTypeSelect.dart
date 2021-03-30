2e62f9951ff70ae1052855054ecc6845ad57c219
GGolfz <wisarutgolf11@gmail.com>
2021-03-31 00:11:16 +0700

    change: type select color :sparkles:

--- lib/widgets/category/categoryTypeSelect.dart
+++ lib/widgets/category/categoryTypeSelect.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:setthi/config/color.dart';
 import 'package:setthi/config/style.dart';
 import 'package:setthi/model/categoryStatus.dart';
 
@@ -16,8 +17,8 @@ class CategoryTypeSelect extends StatelessWidget {
           text,
           textAlign: TextAlign.center,
           style: current == type
-              ? kHeadline3Black.copyWith(fontSize: 18)
-              : kHeadline4Black.copyWith(fontSize: 16),
+              ? kHeadline3Black.copyWith(fontSize: 18, color: kNeutralBlack)
+              : kHeadline4Black.copyWith(fontSize: 16, color: kNeutral450),
         ),
         onTap: () => changeStatus(type),
       ),
