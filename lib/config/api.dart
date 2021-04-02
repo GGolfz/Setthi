6100b0a9fc2a649023f04e4a680a6183244a9223
GGolfz <wisarutgolf11@gmail.com>
2021-04-03 02:43:55 +0700

    change: api endpoint to production :sparkles:

--- lib/config/api.dart
+++ lib/config/api.dart
@@ -1,2 +1,2 @@
-// const apiEndpoint = "https://setthi.cscms.me/api";
-const apiEndpoint = "http://localhost:3000/api";
+const apiEndpoint = "https://setthi.cscms.me/api";
+// const apiEndpoint = "http://localhost:3000/api";
