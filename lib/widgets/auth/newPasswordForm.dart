62a2f442ab726b2df7727b6955bf8ef7dcecc442
boyplus <ict224bj@gmail.com>
2021-04-06 00:42:37 +0700

    update: forget password validator

--- lib/widgets/auth/newPasswordForm.dart
+++ lib/widgets/auth/newPasswordForm.dart
@@ -9,7 +9,9 @@ import 'package:setthi/widgets/auth/authTextField.dart';
 import 'package:setthi/widgets/buttons/primaryButton.dart';
 import 'package:setthi/widgets/buttons/secondaryButton.dart';
 import 'package:setthi/provider/authenicateProvider.dart';
+import 'package:setthi/widgets/layout/customDialog.dart';
 import 'package:setthi/widgets/layout/errorDialog.dart';
+import 'package:setthi/widgets/buttons/actionButton.dart';
 
 class NewPasswordForm extends StatefulWidget {
   final Function changeModal;
@@ -25,17 +27,68 @@ class _NewPasswordFormState extends State<NewPasswordForm> {
   final _formKey = GlobalKey<FormState>();
   bool _isLoading = false;
 
+  Widget _buildResetCompleteDialog(BuildContext context) {
+    return Wrap(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Reset password successfully',
+                    style: TextStyle(color: kGold500, fontSize: 16),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: ActionButton(
+                  text: "Ok",
+                  color: kGold400,
+                  onPressed: () {
+                    Navigator.pop(context);
+                    widget.changeModal(AuthType.signin);
+                  },
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  bool validate() {
+    return _password.text.isNotEmpty &&
+        _confirmPassword.text.isNotEmpty &&
+        _password.text == _confirmPassword.text;
+  }
+
   void changePassword(BuildContext context) async {
-    final authProvider =
-        Provider.of<AuthenticateProvider>(context, listen: false);
-    try {
+    print(_password.text);
+    print(_confirmPassword.text);
+    if (validate()) {
+      final auth = Provider.of<AuthenticateProvider>(context, listen: false);
       setState(() => _isLoading = true);
-      await authProvider.changePassword(_password.text);
-      setState(() => _isLoading = false);
-      widget.changeModal(AuthType.signin);
-    } on HttpException catch (error) {
-      showErrorDialog(context: context, text: error.message);
-      setState(() => _isLoading = false);
+      try {
+        await auth.changePassword(_password.text);
+        setState(() => _isLoading = false);
+        showCustomDialog(
+            context: context, content: _buildResetCompleteDialog(context));
+      } on HttpException catch (error) {
+        showErrorDialog(context: context, text: error.message);
+        setState(() => _isLoading = false);
+      }
+    } else {
+      if (_password.text.isEmpty || _confirmPassword.text.isEmpty) {
+        showErrorDialog(context: context, text: 'Password cannot be empty.');
+      } else {
+        showErrorDialog(context: context, text: 'Password does not match.');
+      }
     }
   }
 
