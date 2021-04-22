280cea2d472ab0d50fb9698b51a4919587aefe08
GGolfz <wisarutgolf11@gmail.com>
2021-04-22 16:22:18 +0700

    init: report screen :sparkles:

---
+++ lib/screens/reportScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ReportScreen extends StatefulWidget {
+  @override
+  _ReportScreenState createState() => _ReportScreenState();
+}
+
+class _ReportScreenState extends State<ReportScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
